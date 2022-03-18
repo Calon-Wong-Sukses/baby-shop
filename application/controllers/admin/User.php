a299e6edc780d8935bba10f48f86cb60901f00ad
Bintang Ramadhan <bintang4974.bintangramadhan4974@gmail.com>
2022-03-19 01:13:35 +0700

    adding modul crud user

--- application/controllers/admin/User.php
+++ application/controllers/admin/User.php
@@ -35,6 +35,82 @@ class User extends CI_Controller
         $this->load->view('admin/user/tambah_user');
         $this->load->view('templates/footer');
     }
+
+    public function tambah_aksi()
+    {
+        $this->_rules();
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->tambah();
+        } else {
+            $data = array(
+                'username'   => $this->input->post('username'),
+                'password'   => $this->input->post('password'),
+                'no_telp'   => $this->input->post('no_telp'),
+                'email'   => $this->input->post('email'),
+                'role'   => $this->input->post('role'),
+            );
+
+            $this->inventory_model->insert_data($data, 'user');
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">Data Berhasil Ditambahkan!<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+            redirect('admin/user');
+        }
+    }
+
+    public function edit_data($id)
+    {
+        $data['title'] = 'Edit User';
+        $data['user'] = $this->db->query("SELECT * FROM user WHERE id_user='$id'")->result();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('admin/user/edit_user', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function edit_aksi()
+    {
+        $this->_rules();
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->tambah();
+        } else {
+
+            $data = array(
+                'username'   => $this->input->post('username'),
+                'password'   => $this->input->post('password'),
+                'no_telp'   => $this->input->post('no_telp'),
+                'email'   => $this->input->post('email'),
+                'role'   => $this->input->post('role'),
+            );
+
+            $where = array(
+                'id_user' => $this->input->post('id_user'),
+            );
+
+            $this->inventory_model->update_data('user', $data, $where);
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">Data Berhasil Diubah!<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+            redirect('admin/user');
+        }
+    }
+
+    public function _rules()
+    {
+        $this->form_validation->set_rules('username', 'Username', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        $this->form_validation->set_rules('no_telp', 'No Telp', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required');
+        $this->form_validation->set_rules('role', 'Role', 'required');
+    }
+
+    public function delete_data($id)
+    {
+        $where = array('id_user' => $id);
+
+        $this->inventory_model->delete_data($where, 'user');
+        $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">Data Berhasil Dihapus!<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+        redirect('admin/user');
+    }
 }
 
 /* End of file User.php */
